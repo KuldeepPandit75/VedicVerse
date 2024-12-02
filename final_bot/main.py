dfec27d26f63649784ab21c5dfe38b858589ddc6
KuldeepPandit75 <kuldeepk11139@gmail.com>
2024-12-02 19:30:50 +0530

    adding bot

---
+++ final_bot/main.py
@@ -0,0 +1,50 @@
+from flask import Flask, render_template, request, jsonify, session
+from flask_cors import CORS  # Import CORS
+import os
+import google.generativeai as genai
+
+# Configure the API key
+genai.configure(api_key="AIzaSyCRRWanA8UnO1agrOflq-IN2nTnJE9S6Gs")
+
+# Create the model configuration
+generation_config = {
+  "temperature": 1,
+  "top_p": 0.95,
+  "top_k": 64,
+  "max_output_tokens": 8192,
+  "response_mime_type": "text/plain",
+}
+
+# Global variable to store the chat session
+chat_session = None
+
+app = Flask(__name__)
+app.secret_key = "@1212324dsfdsagjhgjhgjhgjhgjhgjgj"  # Necessary if using Flask session
+
+CORS(app, resources={r"/predictu": {"origins": "http://localhost:5173"}})
+
+#@app.before_request
+#def start_chat_session():
+chat_session = None
+if chat_session is None:
+    model = genai.GenerativeModel(
+        model_name="gemini-1.5-flash",
+        generation_config=generation_config,
+
+        #VedicVerse Instruction
+
+        system_instruction="user will give you a sanskrit shlok you just need to give its deep and detailed and full translation in hindi nothing else no greeting no being smart no improv.",
+    )
+    chat_session = model.start_chat(history=[])
+
+@app.post("/predictu")
+def predictu():
+    global chat_session
+    text = request.get_json().get("message")
+    response = chat_session.send_message(text)
+    message = {"answer": response.text}
+    print(message)
+    return jsonify(message)
+
+if __name__ == "__main__":
+    app.run(debug=True)
