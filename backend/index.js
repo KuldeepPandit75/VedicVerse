59f213df8e25ede17e64b21d4f2a4d55d5bbb459
kuldeepchaudhary75 <choudharykuldeep5511@gmail.com>
2025-01-17 20:37:34 +0530

    adding cors

--- backend/index.js
+++ backend/index.js
@@ -15,12 +15,11 @@ const port = process.env.PORT || 3030;
 
 // app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-app.use(cors());
+app.use(cors({ origin: "https://mercat-ten.vercel.app" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use("/", router);
-// app.use("/api/quizzes", quizRoutes);
 app.listen(port, () => {
   console.log(`listening on port http://localhost:${port}`);
 });
