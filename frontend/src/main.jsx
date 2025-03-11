0c7ca176ad476bf5dc701c0df20c8c240ed62be3
kuldeepchaudhary75 <choudharykuldeep5511@gmail.com>
2025-03-11 23:48:25 +0530

    change in main.jsx created new file index.js in components

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,22 +8,28 @@ import {
   RouterProvider,
 } from "react-router";
 import { Provider } from "react-redux";
+
 import store from "./App/store.js";
-import Login from "./components/login/Login.jsx";
 import Layout from "./Layout.jsx";
-import Meta from "./components/meta/Meta.jsx";
-import Home from "./components/home/Home.jsx";
-import PrivateRoute from "./components/PrivateRoute.jsx";
-import BookContainer from "./components/Book/BookContainer.jsx";
-import SelectBook from "./components/Book/selectBook.jsx";
-import Translate from "./components/translate/Translate.jsx";
-import Translate2 from "./components/translate/Translate2.jsx";
-import VedBooks from "./components/vedBooks/VedBooks.jsx";
-import PDFViewer from "./components/Book/Book2.jsx";
-// import ThreeCanvas from "./components/fire/ThreeCanvas.jsx";
-import Intro from "./components/features/Intro.jsx";
-import Sparks from "./components/features/FireSparkles.jsx";
-import QuizPage from "./components/Quiz/quiz.jsx";
+
+import {
+  Login,
+  Meta,
+  Home,
+  PrivateRoute,
+  BookContainer,
+  SelectBook,
+  Translate,
+  Translate2,
+  VedBooks,
+  PDFViewer,
+  // ThreeCanvas,
+  Intro,
+  Sparks,
+  QuizPage,
+} from "./components/index.js";
+
+
 function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
