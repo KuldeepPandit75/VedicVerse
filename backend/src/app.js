c771957b5d8a956bc0caae21ec9bf4b3897ef983
KuldeepPandit75 <kuldeepk11139@gmail.com>
2025-04-13 01:23:40 +0530

    frontend hosted cors added

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173","https://vedic-verse.vercel.app"],
     credentials: true,
   })
 );
